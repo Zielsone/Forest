7dc0ca616edf291252414c069297072b8b69ee01
Bartek <zielsone@gmail.com>
2023-12-29 18:12:32 +0100

    Adding a new content and improving the menu section

--- src/js/main.js
+++ src/js/main.js
@@ -1,27 +1,33 @@
-const nav = document.querySelector('.nav');
-const navBtn = document.querySelector('.burger-btn');
-const allNavItems = document.querySelectorAll('.nav__item');
+const nav = document.querySelector(".nav");
+const navBtn = document.querySelector(".burger-btn");
+const allNavItems = document.querySelectorAll(".nav__item");
 
 const handleNav = () => {
-    nav.classList.toggle('nav--active')
+  nav.classList.toggle("nav--active");
+  if (nav.classList.contains("nav--active")) {
+    document.body.style.overflow = "hidden";
+  } else {
+    document.body.style.overflow = "visible";
+  }
 
-    allNavItems.forEach(item => {
-        item.addEventListener('click', () =>{
-            nav.classList.remove('nav--active')
-        })
+  allNavItems.forEach((item) => {
+    item.addEventListener("click", () => {
+      nav.classList.remove("nav--active");
+      document.body.style.overflow = "visible";
     });
+  });
 
-    handleNavItemsAnimation();
-}
+  handleNavItemsAnimation();
+};
 
 const handleNavItemsAnimation = () => {
-    let delayTime = 0;
+  let delayTime = 0;
 
-    allNavItems.forEach(item => {
-        item.classList.toggle('nav-items-animation')
-        item.style.animationDelay = '.' + delayTime + 's';
-        delayTime ++;
-    })
-}
+  allNavItems.forEach((item) => {
+    item.classList.toggle("nav-items-animation");
+    item.style.animationDelay = "." + delayTime + "s";
+    delayTime++;
+  });
+};
 
-navBtn.addEventListener('click', handleNav);
+navBtn.addEventListener("click", handleNav);
